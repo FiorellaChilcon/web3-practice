4a64bcdb564ede909e6df9a00a6a0da58bb608de
fiorellachilcon <fiorellachilc_20@hotmail.com>
2021-06-21 17:11:57 -0500

    cryptocurrency/token contract

---
+++ contracts/coin.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.4;
+
+contract BananaCoin {
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+  uint256 public totalSuply;
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
+
+  constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+    name = _name;
+    symbol = _symbol;
+    totalSuply = _totalSupply;
+    decimals = 18;
+  }
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[_from] >= _value);
+    require(allowance[_from][msg.sender] >= _value);
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+
+    allowance[_from][msg.sender] -= _value;
+
+    emit Transfer(_from, _to, _value);
+
+    return true;
+  }
+}
